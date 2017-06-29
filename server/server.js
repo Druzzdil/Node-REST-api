7a588c693c27d7a65adc14e0c2a6f9e91274f7cd
Druzzdil <staniszewski86@gmail.com>
2017-06-29 19:31:58 +0200

    add email validation

--- server/server.js
+++ server/server.js
@@ -12,10 +12,12 @@ let  Todo = mongoose.model('Todo', {
         trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
@@ -27,5 +29,44 @@ newTodo.save().then((doc)=>{
 }, (err)=>{
     console.log('unable to save todo', err);
 });
+
+let validateEmail = function(email) {
+    let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+};
+
+function reg(email){
+    let ara = ['end is night'];
+    let res = email.test(ara);
+    console.log(res, 'tested regex');
+}
+
+
+let User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength :3,
+        trim: true,
+        unique: true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    }
+});
+
+let newUser = new User({
+   email: '  staniszewski8.apd  ',
+});
+
+
+
+newUser.save().then((docs)=>{
+    console.log(JSON.stringify(docs, undefined, 2))
+}, (err)=>{
+    console.log('unable to save email address', err)
+});
+
+
+
 
 
