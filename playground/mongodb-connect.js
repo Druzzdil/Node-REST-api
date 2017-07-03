1f11d74c78d35babf2f4f8340f612af466440502
Druzzdil <staniszewski86@gmail.com>
2017-07-03 20:28:34 +0200

    add patch todos/id

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,7 +1,6 @@
 
 const {MongoClient, ObjectID} = require('mongodb');
 
-
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db )=>{
     if(err){
        return console.log('unable to mongodb server');
--- server/server.js
+++ server/server.js
@@ -40,7 +40,6 @@ app.get('/todos', function(req,res){
 });
 
 
-
 app.get('/todos/:id', (req,res)=>{
   let id = req.params.id;
   if (!ObjectID.isValid(id)) {
@@ -74,6 +73,34 @@ app.delete('/todos/:id', (req, res)=>{
     }).catch((err) => {
       console.log(err);
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(port, () => {
