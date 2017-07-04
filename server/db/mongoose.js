5502d7cfab62de13128664f2c9be7569e16ff92e
Druzzdil <staniszewski86@gmail.com>
2017-07-04 13:06:35 +0200

    up

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,11 @@
-
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.MONGODB_URI );
+//mongoose.connect( process.env.MONGODB_URI );
 module.exports = { mongoose };
+
+
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://SentientBreath:lorien117@ds147052.mlab.com:47052/todos'
+};
+mongoose.connect(db.mlab || db.localhost);
