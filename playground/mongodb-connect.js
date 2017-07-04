4d8c7d6dd021d3dde6a1d52d23aa147d8c8ee724
Druzzdil <staniszewski86@gmail.com>
2017-07-04 13:25:15 +0200

    update

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,20 +1,16 @@
 
 const {MongoClient, ObjectID} = require('mongodb');
-
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db )=>{
     if(err){
        return console.log('unable to mongodb server');
     }
     console.log('connected to mongodb');
-    db.collection('Todos').insertOne({
-        text: 'the night is beautifull33',
-        completed: false,
-        number: 99
-    }, (err, result)=>{
-        if (err){
-            return console.log('unable to insert todo', err)
-        }
-        console.log(result.ops[0]._id.toString());
+    db.collection('todos').insertOne({
+        text: 'the night is beautifull'
+    }).then(function(result) {
+        console.log('inserted', result);
     });
-    db.close()
+    db.close();
 });
