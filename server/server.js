ffd4e80134f500f5cbafa38b923c9c98552c0d8e
Druzzdil <staniszewski86@gmail.com>
2017-07-04 12:07:45 +0200

    server connection update

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+let env = process.env.NODE_ENV || 'development';
+
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
@@ -78,25 +80,19 @@ app.delete('/todos/:id', (req, res)=>{
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
-
-
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-
   if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
   } else {
     body.completed = false;
     body.completedAt = null;
   }
-
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-
     if (!todo) {
       return res.status(404).send();
     }
-
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
